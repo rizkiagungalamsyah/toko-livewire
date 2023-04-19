353e443950ae0a49c8eff0e4160398dff652a835
rizkiagungalamsyah <rizkiagung028@gmail.com>
2023-04-19 23:26:02 +0700

    feat: add search product

--- app/Http/Livewire/Product/Index.php
+++ app/Http/Livewire/Product/Index.php
@@ -10,10 +10,24 @@ class Index extends Component
 {
     use WithPagination;
 
+    public $paginate = 10;
+    public $search;
+
+    protected $updatesQueryString = [
+        ['search' => ['except' => '']],
+    ];
+
+    public function mount()
+    {
+        $this->search = request()->query('search', $this->search);
+    }
+
     public function render()
     {
         return view('livewire.product.index', [
-            'products' => Product::latest()->paginate(10)
+            'products' => $this->search === null ?
+                Product::latest()->paginate($this->paginate) :
+                Product::latest()->where('title', 'like', '%' . $this->search . '%')->paginate($this->paginate)
         ]);
     }
 }
