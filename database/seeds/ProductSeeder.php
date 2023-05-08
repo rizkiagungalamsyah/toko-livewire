98e58e0f6b21e8ab7d9767eb32a44e3fd65d36a5
rizkiagungalamsyah <rizkiagung028@gmail.com>
2023-05-09 02:00:26 +0700

    Update

--- database/seeds/ProductSeeder.php
+++ database/seeds/ProductSeeder.php
@@ -12,6 +12,6 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-        factory(Product::class, 50)->create();
+        // factory(Product::class, 50)->create();
     }
 }
